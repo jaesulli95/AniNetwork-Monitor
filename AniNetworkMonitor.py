647e59f24f64e9f5a1524873199264f4a32358b2
Jaesulli95 <jsulli0527@gmail.com>
2018-04-28 17:10:10 -0400

    Added Comments, Functionality now requires the magnet_link to work, and add it via that hash and then will be able to easily monitor that torrent.

--- AniNetworkMonitor.py
+++ AniNetworkMonitor.py
@@ -5,20 +5,22 @@ import time
 
 
 class AniNetworkMonitor():
-    def __init__(self, id):
+    def __init__(self, tml):
         self.r = requests.Session()
-        self.tor_id = id
+        self.tor_id = None
         self.tor_info = {}
         self.session_cookie = ""
+        self.tor_mag_link = tml
 
     def run(self):
         self.login()
+        self.add_torrent()
         while True:
             self.tor_info = self.get_torrent_info()
             if len(self.tor_info) == 0:
                 break
             if self.tor_info["is_finished"]:
-                self.remove_torrent()
+                self.remove_torrent(self.tor_id)
                 break
             else:
                 time.sleep(self.tor_info['eta'])
@@ -34,12 +36,23 @@ class AniNetworkMonitor():
         data = {"id": 1, "method": DELUGE_GET_TORRENT, "params": [self.tor_id, {}]}
         self.r = requests.post(URL, data=json.dumps(data), headers=HEADERS, cookies={'_session_id': self.session_cookie})
         return json.loads(self.r.content)['result']
-
-    def remove_torrent(self):
-        remove_data = {"id": 1, "method": DELUGE_REMOVE_TORRENT, "params": [self.tor_id, ""]}
+    """
+        Remove the torrent identified by it's hash from the deluge client, and then allow for it to be moved.
+    """
+    def remove_torrent(self, id):
+        remove_data = {"id": 1, "method": DELUGE_REMOVE_TORRENT, "params": [id, ""]}
         self.r = requests.post(URL, data=json.dumps(remove_data), headers=HEADERS, cookies={'_session_id': self.session_cookie})
-
+    """
+        Login into the API, and deluge torrent set.
+    """
     def login(self):
         login_data = {"id": 1, "method": DELUGE_LOGIN, "params":["deluge"]}
         self.r = requests.post(URL, data=json.dumps(login_data), headers=HEADERS)
         self.session_cookie = self.r.headers['Set-Cookie'].split(";")[0].split("=")[1]
+    """
+        Add Torrent to be downloaded, using the magnet link.
+    """
+    def add_torrent(self):
+        add_data = {"id":1, "method": DELUGE_ADD_TORRENT, "params": [self.tor_mag_link, {}]}
+        self.r = requests.post(URL, data=json.dumps(add_data), headers=HEADERS, cookies={'_session_id': self.session_cookie})
+        self.tor_id = json.loads(self.r.content)['result']
